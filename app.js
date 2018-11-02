e4efb9f3f04f7a833f770af8e4981d94a6b8763b
Unknown <mattimatti2000@gmail.com>
2018-11-02 15:38:56 +0100

    ready for deploy

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const delay = require('delay');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
-const port = 3000;
+const port = 80;
 
 
 
@@ -65,7 +66,11 @@ app.get('/', function (req,res) {
 
         });
     });
-    res.render('index', {data: count});
+    (async () => {
+        await delay(600);
+        res.render('index', {data: count});
+    })();
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
